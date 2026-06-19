742edc4decc7a1120438920eb2cb4b40176f1763
Ishrak-1520 <your_email@example.com>
2026-06-19 22:55:35 +0600

    fix: move menu_views to bottom of manifest to fix unresolvable action error

--- addons/nn_fund_management/__manifest__.py
+++ addons/nn_fund_management/__manifest__.py
@@ -44,7 +44,6 @@ Approval Workflow:
         'data/sequence_data.xml',
 
         # Views
-        'views/menu_views.xml',
         'views/fund_account_views.xml',
         'views/fund_incoming_views.xml',
         'views/fund_allocation_views.xml',
@@ -55,6 +54,7 @@ Approval Workflow:
         'views/fund_transfer_views.xml',
         'views/approval_history_views.xml',
         'views/dashboard_views.xml',
+        'views/menu_views.xml',
     ],
     'demo': [
         'data/demo_data.xml',
